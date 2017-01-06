71a1f9e718c6d99ed8138411ecc8173fd460b2f9
akosthurzo <akos.thurzo@liferay.com>
2017-01-06 20:44:28 +0100

    Add course.js (create, delete)

---
+++ src/main/webapp/js/course.js
@@ -0,0 +1,46 @@
+(function() {
+    var app = angular.module("elephantModule", []);
+
+    var courseController = function($scope, $http) {
+        $scope.course = {};
+
+        $scope.refreshCourses = function() {
+            $http.get('/api/courses')
+                 .then(function successCallback(response) {
+                          console.log("SUCCESS! " + response);
+                          $scope.courses = response.data._embedded.courses;
+                       },
+                       function errorCallback(response) {
+                          console.log("ERROR! " + response);
+                       });
+
+            return null;
+        };
+
+        $scope.createCourse = function() {
+            $http.post('/api/courses', $scope.course)
+                 .then(function successCallback(response) {
+                         console.log("SUCCESS! " + response);
+                         $scope.refreshCourses();
+                     },
+                     function errorCallback(response) {
+                         console.log("ERROR! " + response);
+                     });
+        };
+
+        $scope.deleteCourse = function(id) {
+            $http.delete('/api/courses/' + id)
+                 .then(function successCallback(response) {
+                          console.log("SUCCESS! " + response);
+                          $scope.refreshCourses();
+                       },
+                       function errorCallback(response) {
+                          console.log("ERROR! " + response);
+                       });
+        };
+
+        $scope.refreshCourses();
+    };
+
+    app.controller("courseController", ['$scope', '$http', courseController]);
+})();
