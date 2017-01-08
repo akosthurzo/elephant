9f5a45e4546dc2acfc7d7d1e6333441b6bf0c698
akosthurzo <akos.thurzo@liferay.com>
2017-01-08 19:33:57 +0100

    Add course functionality moved to a new service

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,3 +1,17 @@
-(function() {
-    var app = angular.module("elephantApp", []);
+(function () {
+   var app = angular.module("elephantApp", []);
+
+   app.factory("courseService", ['$http', '$q', function ($http, $q) {
+      return {
+         addCourse: function (course) {
+            return $http.post('/api/courses', course)
+               .then(function successCallback(response) {
+                     console.log("addCourse SUCCESS! " + response);
+                  },
+                  function errorCallback(response) {
+                     console.log("addCourse ERROR! " + response);
+                  });
+         },
+      };
+   }]);
 })();
