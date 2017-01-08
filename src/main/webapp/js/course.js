3581081988347ea80f57cc7a449d4d876f912117
akosthurzo <akos.thurzo@liferay.com>
2017-01-08 19:36:22 +0100

    Update logic to use the new courseService

--- src/main/webapp/js/course.js
+++ src/main/webapp/js/course.js
@@ -4,43 +4,35 @@
    var courseController = function ($scope, $http, courseService) {
       $scope.course = {};
 
-        $scope.refreshCourses = function() {
-            $http.get('/api/courses')
-                 .then(function successCallback(response) {
-                          console.log("SUCCESS! " + response);
-                          $scope.courses = response.data._embedded.courses;
-                       },
-                       function errorCallback(response) {
-                          console.log("ERROR! " + response);
-                       });
+      $scope.refreshCourses = function () {
+         $http.get('/api/courses')
+            .then(function successCallback(response) {
+                  console.log("getAllCourses SUCCESS! " + response);
+                  $scope.courses = response.data._embedded.courses;
+               },
+               function errorCallback(response) {
+                  console.log("getAllCourses ERROR! " + response);
+               });
+      };
 
-            return null;
-        };
+      $scope.addCourse = function () {
+         courseService.addCourse($scope.course)
+            .then(function () {
+               $scope.refreshCourses();
+            });
 
-        $scope.createCourse = function() {
-            $http.post('/api/courses', $scope.course)
-                 .then(function successCallback(response) {
-                         console.log("SUCCESS! " + response);
-                         $scope.refreshCourses();
-                     },
-                     function errorCallback(response) {
-                         console.log("ERROR! " + response);
-                     });
-        };
+         $scope.course = {};
+      };
 
-        $scope.deleteCourse = function(id) {
-            $http.delete('/api/courses/' + id)
-                 .then(function successCallback(response) {
-                          console.log("SUCCESS! " + response);
-                          $scope.refreshCourses();
-                       },
-                       function errorCallback(response) {
-                          console.log("ERROR! " + response);
-                       });
-        };
+      $scope.deleteCourse = function (id) {
+         courseService.deleteCourse(id)
+            .then(function () {
+               $scope.refreshCourses();
+            });
+      };
 
-        $scope.refreshCourses();
-    };
+      $scope.refreshCourses();
+   };
 
    app.controller("courseController", ['$scope', '$http', 'courseService', courseController]);
 })();
