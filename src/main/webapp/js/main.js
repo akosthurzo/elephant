24109a0b2ec4d802a9066bebf883f1c5f9b7221d
akosthurzo <akos.thurzo@liferay.com>
2017-01-08 20:15:54 +0100

    Add ng-route

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,3 +1,11 @@
 (function () {
-   var app = angular.module("elephantApp", []);
+   var app = angular.module("elephantApp", ["ngRoute"]);
+
+   app.config(function ($routeProvider) {
+      $routeProvider
+         .when("/main", {
+            templateUrl: "index.html"
+         })
+         .otherwise({redirectTo: "/main"});
+   });
 })();
