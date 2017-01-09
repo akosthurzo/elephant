5a0db2c7461eeac458f589a6a1b048a42b277591
akosthurzo <akos.thurzo@liferay.com>
2017-01-09 22:22:50 +0100

    Load course details

--- src/main/webapp/js/course_details.js
+++ src/main/webapp/js/course_details.js
@@ -1,11 +1,20 @@
 (function () {
    var app = angular.module("elephantApp");
 
-   var courseDetailsController = function ($scope, $http, $routeParam, courseService) {
-      $scope.course = {};
+   var courseDetailsController = function ($scope, $http, $routeParams, courseService) {
+      $scope.course = {
+         id: $routeParams.id
+      };
 
+      courseService.getCourse($scope.course.id)
+         .then(function(course) {
+            $scope.course = course;
+         });
 
+      $scope.updateCourse = function() {
+
+      };
    };
 
-   app.controller("courseDetailsController", ['$scope', '$http', '$routeParam', 'courseService', courseDetailsController]);
+   app.controller("courseDetailsController", ['$scope', '$http', '$routeParams', 'courseService', courseDetailsController]);
 })();
