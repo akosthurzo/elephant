ac91a40893fbd4a29b5b345bfcc3658c5a2ef5fe
akosthurzo <akos.thurzo@liferay.com>
2017-01-17 17:06:32 -0800

    Add ability to find cards by dueDate and course id

--- src/main/java/com/elephantapp/repository/CardRepository.java
+++ src/main/java/com/elephantapp/repository/CardRepository.java
@@ -20,4 +20,8 @@ public interface CardRepository extends CrudRepository<Card, Long> {
     @Query("select c from Card c " +
             "where c.module.id = ?1 and c.dueDate between ?2 and ?3")
     List<Card> findByModuleAndDueDateBetween(@Param("module_id") Long module_id, @DateTimeFormat(pattern = "yyyy-MM-dd") @Param("min") Date date1, @DateTimeFormat(pattern = "yyyy-MM-dd") @Param("max") Date date2);
+
+    @Query("select c from Card c " +
+            "where c.module.course.id = ?1 and c.dueDate between ?2 and ?3")
+    List<Card> findByCourseAndDueDateBetween(@Param("module_id") Long module_id, @DateTimeFormat(pattern = "yyyy-MM-dd") @Param("min") Date date1, @DateTimeFormat(pattern = "yyyy-MM-dd") @Param("max") Date date2);
 }
